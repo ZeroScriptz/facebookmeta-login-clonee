21f253cf1f2b4fc942c6086d3fefb2ac1ee68ac3
Zero <110802852+ZeroScriptz@users.noreply.github.com>
2023-01-24 05:39:01 -0700

    type writer added, update packages, update to data such as html meta

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className=''>
         <img
         src='https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg'
-        className='object-scale-down object-left md:object-scale-down' 
+        className='object-scale-down object-left FBLogo' 
         alt=''
         />
         
